14132e2f3de3b591133d74cea9dfaba8b9bc9f6c
lk23286-lgl <lk23286@gmail.com>
2026-04-01 18:52:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.environ.get("API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+# print(f"auth_token: {auth_token}")
+# print(f"api_key: {api_key}")
+# print(f"account_sid: {account_sid}")
+client = Client(account_sid, auth_token)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
+
+
+
+weather_params = {
+    "lat" : 47.597530,
+    "lon" : 19.348030,
+    "appid" : api_key
+}
+
+response = requests.get(url = OWM_Endpoint, params = weather_params)
+#print(response.status_code)
+data = response.json()
+response.raise_for_status()
+#print(data)
+#print(json.dumps(data, indent=4))
+id = data["weather"][0]["id"]
+if id < 700:
+    #print("Bring Umbrella")
+    message = client.messages.create(
+        messaging_service_sid='MG1a843f9cb83bdcea99c7a35a858705fd',
+        body='Bring Umbrella! ☔️',
+        to='+36309843856'
+    )
+    #print(message.sid)
+
+#print(f"id:{id}")
