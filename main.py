e5e49916b9fd109d137c3582d3213c687c5e1eab
lk23286-lgl <lk23286@gmail.com>
2026-04-02 07:03:32 +0200

    remove comments and prints main.py

--- main.py
+++ main.py
@@ -6,17 +6,9 @@ api_key = os.environ.get("API_KEY")
 account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 
-
-# print(f"auth_token: {auth_token}")
-# print(f"api_key: {api_key}")
-# print(f"account_sid: {account_sid}")
 client = Client(account_sid, auth_token)
 
-
-
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
-
-
 
 weather_params = {
     "lat" : 47.597530,
@@ -25,19 +17,15 @@ weather_params = {
 }
 
 response = requests.get(url = OWM_Endpoint, params = weather_params)
-#print(response.status_code)
+
 data = response.json()
 response.raise_for_status()
-#print(data)
-#print(json.dumps(data, indent=4))
+
 id = data["weather"][0]["id"]
 if id < 900:
-    #print("Bring Umbrella")
     message = client.messages.create(
         messaging_service_sid='MG1a843f9cb83bdcea99c7a35a858705fd',
         body='Bring Umbrella! ☔️',
         to='+36309843856'
     )
-    #print(message.sid)
 
-#print(f"id:{id}")
